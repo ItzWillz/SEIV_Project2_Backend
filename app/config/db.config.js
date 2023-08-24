6a83a5c21df9363a3b591c586cd82a0f3a3c209e
ItzWillz <william.x.mcdonald@eagles.oc.edu>
2023-08-24 16:29:39 -0500

    DB&OtherFileSetUp
    Database and other files added and set up. Hopefully properly.

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: '',
+    DB: 'se4p2',
+    dialect: 'mariadb',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
